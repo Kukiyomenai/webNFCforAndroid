778a8da81538d0463bf8893b689ec793e5c3b301
KanaMoji3 <135625231+KanaMoji3@users.noreply.github.com>
2023-10-28 23:12:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ document.getElementById('writeForm').addEventListener('submit', async (e) => {
         const textToWrite = document.getElementById('textWrite').value;
 
         // 1. NDEF メッセージを作成
-        const message = new NDEFMessage([new NDEFTextRecord(textToWrite)]);
+        //const message = new NDEFMessage([new NDEFTextRecord(textToWrite)]);
 
         // 2. NFC リーダーのインスタンスを作成
         const ndef = new NDEFWriter();
 
         // 3. Sony RC-S380 デバイスと通信を確立
-        await ndef.write(usbDevice, message);
+        await ndef.write(usbDevice, textToWrite);
 
         console.log('NFC タグにデータを書き込みました:', textToWrite);
     } catch (error) {
