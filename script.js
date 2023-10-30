708d2478e231ff6b7b884fbd2314904cb1babce4
KanaMoji3 <135625231+KanaMoji3@users.noreply.github.com>
2023-10-31 00:04:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -28,10 +28,17 @@ document.getElementById('writeForm').addEventListener('submit', async (e) => {
     try {
         const url = document.getElementById('urlWriteID').value; // フォームから入力されたテキストを取得
 
+        // NFCリーダーを有効化
+        const ndef = new NDEFReader();
+
+        // NFCリーダーがNFCタグを検出するのを待つ
+        await ndef.scan();
+
         // 入力されたテキストをNFCタグに書き込む
-        await nfc.write({
-            records: [{ recordType: "url", data: url }]
+        await ndef.write({
+            records: [{ recordType: "url", data: url }], // 入力されたテキストをNFCタグに書き込む
         });
+        // URLの形式はhttps://から書き始める必要がある。　例：https://www.google.com
 
         // 書き込んだデータを表示
         writtenDataElement.textContent = '書き込んだデータ: ' + url;
