3aa62f6ac4484c0999fe33d9cd1ed5b266aa1187
KanaMoji3 <135625231+KanaMoji3@users.noreply.github.com>
2023-10-31 00:01:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //HTMLロード時実行
 window.addEventListener('DOMContentLoaded', function () {
+
     const nfcStatusElement = document.getElementById('nfcStatus'); // 状態を表示するための要素
 
     try {
@@ -22,20 +23,19 @@ window.addEventListener('DOMContentLoaded', function () {
 document.getElementById('writeForm').addEventListener('submit', async (e) => {
     e.preventDefault(); // デフォルトのフォーム送信を防ぎます
 
+    const writtenDataElement = document.getElementById('writtenData'); // 書き込んだデータを表示する要素
+
     try {
         const url = document.getElementById('urlWriteID').value; // フォームから入力されたテキストを取得
 
-        // NFCリーダーを有効化
-        const ndef = new NDEFReader();
-
-        // NFCリーダーがNFCタグを検出するのを待つ
-        await ndef.scan();
-
         // 入力されたテキストをNFCタグに書き込む
-        await ndef.write({
-            records: [{ recordType: "url", data: url }], // 入力されたテキストをNFCタグに書き込む
+        await nfc.write({
+            records: [{ recordType: "url", data: url }]
         });
-        // URLの形式はhttps://から書き始める必要がある。　例：https://www.google.com
+
+        // 書き込んだデータを表示
+        writtenDataElement.textContent = '書き込んだデータ: ' + url;
+        writtenDataElement.style.display = 'block'; // データを表示
 
         console.log('NFCタグにデータを書き込みました:', url);
     } catch (error) {
