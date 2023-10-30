c66ae70164d6d849b1f06811d336857ca961b7da
KanaMoji3 <135625231+KanaMoji3@users.noreply.github.com>
2023-10-31 00:13:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-//HTMLロード時実行
-window.addEventListener('DOMContentLoaded', function () {
-
-    const nfcStatusElement = document.getElementById('nfcStatus'); // 状態を表示するための要素
-
-    try {
-        const nfc = new NDEFReader(); // NFCリーダーオブジェクトを作成
-
-        // NFCリーダーの状態を監視
-        nfc.addEventListener('reading', () => {
-            nfcStatusElement.textContent = 'NFCリーダーが有効です';
-        });
-
-        nfc.addEventListener('readingerror', (error) => {
-            nfcStatusElement.textContent = 'NFCリーダーが無効です: ' + error.message;
-        });
-    } catch (error) {
-        nfcStatusElement.textContent = 'NFCリーダーの初期化中にエラーが発生しました: ' + error.message;
-    }
-});
-
 // フォームの送信ボタンがクリックされたときにNFCタグに書き込む処理を実行
 document.getElementById('writeForm').addEventListener('submit', async (e) => {
     e.preventDefault(); // デフォルトのフォーム送信を防ぎます
